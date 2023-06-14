ede408b4ea3a3c2a69dace113db8419c7c69736a
Matt Wuring <matt.wuring@gmail.com>
2023-06-14 15:41:00 +0200

    Header

---
+++ components/header.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="w-full m-auto h-20 bg-white">
+        <div class="container">
+            <div class="flex justify-between h-20 items-center">
+                <div class="text-2xl font-semibold">
+                    Stripe Shop
+                </div>
+                <div class="basket">
+                    Basket
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+    .container{
+        margin-left: 15%;
+        margin-right: 15%;
+        width: 70%;
+    }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,27 @@
 <template>
-    <div class="w-full m-auto">
-        <Product />
-    </div>    
+    <div class="w-full h-screen bg-gray-200">
+        <div class="container">
+            <div class="flex flex-wrap gap-4">
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+                <Product />
+            </div>
+        </div>    
+    </div>
 </template>
 
 <script lang="ts" setup></script>
 
 <style lang="scss" scoped>
-
+    .container{
+        margin-left: 15%;
+        margin-right: 15%;
+        width: 70%;
+    }
 </style>
